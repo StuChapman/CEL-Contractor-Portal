d14137f1736e33ef2b3a2e64e6d388dbdc169a76
Stuart Chapman <chapman.stuart@sky.com>
2023-07-05 14:02:23 +0000

    Working on bug on portal001 views

--- mysite/portal001/urls.py
+++ mysite/portal001/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('portal001/', views.curo, name='curo'),
-    path('portal001/', views.updateorderform, name='updateorderform'),
+    path('portal001/', views.indexes, name='indexes'),
 ]
--- mysite/portal001/views.py
+++ mysite/portal001/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404, HttpResponse
 from django.urls import path
 from django.contrib import messages
-from django.utils.safestring import mark_safe
 from . import views
 from .models import Orders
 from .forms import OrderForm
@@ -13,24 +12,11 @@ from django.db.models import Q
 
 def curo(request):
     """ A view to return the intro page """
-    thisOrder = 1234567
-    queries = (Q(orderNumber__icontains=thisOrder) |
-               Q(orderDescription__icontains=thisOrder))
-    orders = Orders.objects.all()
-    order_list = (orders.filter(queries)
-                  .order_by('orderNumber').first())
-    order_list_length = orders.filter(queries).count()
-    form = OrderForm(instance=order_list)
-    context = {
-        'form': form,
-        'order_list': order_list,
-        'order_list_length': order_list_length,
-    }
-
-    return render(request, 'curo/portal.html', context)
-
-
-def updateorderform(request):
-    """ A view to return the index page """
 
     return render(request, 'curo/home.html')
+
+
+def indexes(request):
+    """ A view to return the index page """
+
+    return render(request, 'home/home.html')
