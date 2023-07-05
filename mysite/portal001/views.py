87debfc55c67cce1973897f8be06c9e37ef9d38c
Stuart Chapman <chapman.stuart@sky.com>
2023-07-05 16:21:22 +0000

    Fixed cock up on portal001 urls.py

--- mysite/portal001/views.py
+++ mysite/portal001/views.py
@@ -7,16 +7,37 @@ from .forms import OrderForm
 from django.db.models import Q
 
 
-# Create your views here.
-
-
 def curo(request):
     """ A view to return the intro page """
+    thisOrder = 1234567
+    queries = (Q(orderNumber__icontains=thisOrder) |
+               Q(orderDescription__icontains=thisOrder))
+    orders = Orders.objects.all()
+    order_list = (orders.filter(queries)
+                  .order_by('orderNumber').first())
+    order_list_length = orders.filter(queries).count()
+    form = OrderForm(instance=order_list)
+    context = {
+        'form': form,
+        'order_list': order_list,
+        'order_list_length': order_list_length,
+    }
 
-    return render(request, 'curo/home.html')
+    return render(request, 'curo/portal.html', context)
 
 
-def indexes(request):
-    """ A view to return the index page """
+def updateOrderForm(request):
+    """ Update the order form """
 
-    return render(request, 'home/home.html')
+    orders = Orders.objects.all()
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+    else:
+        return render(request, 'home/home.html')
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'curo/portal.html', context)
