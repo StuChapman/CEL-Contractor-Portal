42e90ec8ad99b55ad2c8f28f34321b17091792c1
Stuart Chapman <chapman.stuart@sky.com>
2023-07-05 11:24:29 +0000

    Worked on editmodal

--- mysite/static/js/scripts.js
+++ mysite/static/js/scripts.js
@@ -1,6 +1,28 @@
 
 // Function: open edit modal //
 function editOrder(orderfield) {
-    console.log("orderfield");
-    console.log(orderfield);
+    let orderfieldString = "#id_" + orderfield;
+    $('#editModalLabel').html("Edit " + orderfield + " field");
+
+    $('input[name="orderDescription"]').prop('disabled', false);
+}
+
+//Function: replace the appropriate field in portal.html with the text from #question and add a note //
+function saveEdit() {
+    let orderfield = $('#editModalLabel').html();
+    
+    switch(orderfield) {
+        case 'Edit orderDescription field':
+            if (!(/^[a-z A-Z?:,.-]+$/.test(this.editupdate.value)) ||this.editupdate.value.length < 1) {
+                alert('Please enter text only, with no special characters .');
+                this.editupdate.focus();
+                return false;
+            } else {
+                console.log(this.editupdate.value);
+                $('input[name="orderDescription"]').val(this.editupdate.value);
+            }
+            break;
+        default:
+            break;
     }
+}
