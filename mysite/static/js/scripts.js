8fa66e859e797892de6ed91bb196b809e52e70dc
Stuart Chapman <chapman.stuart@sky.com>
2023-07-06 20:06:14 +0000

    Added timestamp  on orderdetail.html

--- mysite/static/js/scripts.js
+++ mysite/static/js/scripts.js
@@ -2,7 +2,36 @@
 // Function: open edit modal //
 function editOrder(orderfield) {
     $('input[name="csrfmiddlewaretoken"]').prop('disabled', false);
-    $('input[name="orderDescription"]').prop('disabled', false);
+
+    switch(orderfield) {
+        case 'orderDescription':
+            $('input[name="orderDescription"]').prop('disabled', false);
+            $('input[name="dateLastUpdate"]').text(today);
+            break;
+        case 'name':
+            $('input[name="name"]').prop('disabled', false);
+            break;
+        case 'address':
+            $('input[name="address"]').prop('disabled', false);
+            break;
+        case 'contractor':
+            $('input[name="contractor"]').prop('disabled', false);
+            break;
+        case 'appointmentDate':
+            $('input[name="appointmentDate"]').prop('disabled', false);
+            break;
+        case 'primaryContact':
+            $('input[name="primaryContact"]').prop('disabled', false);
+            break;
+        case 'secondaryContact':
+            $('input[name="secondaryContact"]').prop('disabled', false);
+            break;
+        default:
+            break;
+    }
+
+
+
     $('input[name="submit"]').prop('disabled', false);
 }
 
@@ -20,4 +49,7 @@ function saveOrder() {
     $('input[name="notes"]').prop('disabled', false);
     $('input[name="dateLastUpdate"]').prop('disabled', false);
     $('input[name="dateCreated"]').prop('disabled', false);
+
+    let today = new Date().toISOString().slice(0, 19)
+    $('input[name="dateLastUpdate"]').val(today);
 }
