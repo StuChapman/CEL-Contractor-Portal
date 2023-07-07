4e777dc687c9be84fb2f2f54d40bd2b794c19cfe
Stuart Chapman <chapman.stuart@sky.com>
2023-07-07 09:34:22 +0000

    Fixed timezone issue with LastUpdated timestamp

--- mysite/static/js/scripts.js
+++ mysite/static/js/scripts.js
@@ -1,42 +1,87 @@
 
+// Global variables //
+var originalText
+var newText
+var updatefield
+
+// Function: auto height textarea //
+function auto_height(elem) {  /* javascript */
+    elem.style.height = "1px";
+    elem.style.height = (elem.scrollHeight)+"px";
+}
+
 // Function: open edit modal //
 function editOrder(orderfield) {
+
+    updatefield = orderfield;
     $('input[name="csrfmiddlewaretoken"]').prop('disabled', false);
 
     switch(orderfield) {
         case 'orderDescription':
             $('input[name="orderDescription"]').prop('disabled', false);
-            $('input[name="dateLastUpdate"]').text(today);
+            originalText = $('input[name="orderDescription"]').val();
             break;
         case 'name':
             $('input[name="name"]').prop('disabled', false);
+            originalText = $('input[name="name"]').val();
             break;
         case 'address':
             $('input[name="address"]').prop('disabled', false);
+            originalText = $('input[name="address"]').val();
             break;
         case 'contractor':
             $('input[name="contractor"]').prop('disabled', false);
+            originalText = $('input[name="contractor"]').val();
             break;
         case 'appointmentDate':
             $('input[name="appointmentDate"]').prop('disabled', false);
+            originalText = $('input[name="appointmentDate"]').val();
             break;
         case 'primaryContact':
             $('input[name="primaryContact"]').prop('disabled', false);
+            originalText = $('input[name="primaryContact"]').val();
             break;
         case 'secondaryContact':
             $('input[name="secondaryContact"]').prop('disabled', false);
+            originalText = $('input[name="secondaryContact"]').val();
             break;
         default:
             break;
     }
 
-
-
+    $("#order-submit").removeClass("hidden");
     $('input[name="submit"]').prop('disabled', false);
 }
 
 // Function: open edit modal //
 function saveOrder() {
+
+    switch(updatefield) {
+        case 'orderDescription':
+            newText = $('input[name="orderDescription"]').val();
+            break;
+        case 'name':
+            newText = $('input[name="name"]').val();
+            break;
+        case 'address':
+            newText = $('input[name="address"]').val();
+            break;
+        case 'contractor':
+            newText = $('input[name="contractor"]').val();
+            break;
+        case 'appointmentDate':
+            newText = $('input[name="appointmentDate"]').val();
+            break;
+        case 'primaryContact':
+            newText = $('input[name="primaryContact"]').val();
+            break;
+        case 'secondaryContact':
+            newText = $('input[name="secondaryContact"]').val();
+            break;
+        default:
+            break;
+    }
+
     $('input[name="csrfmiddlewaretoken"]').prop('disabled', false);
     $('input[name="orderNumber"]').prop('disabled', false);
     $('input[name="orderDescription"]').prop('disabled', false);
@@ -50,6 +95,12 @@ function saveOrder() {
     $('input[name="dateLastUpdate"]').prop('disabled', false);
     $('input[name="dateCreated"]').prop('disabled', false);
 
-    let today = new Date().toISOString().slice(0, 19)
+    const d = new Date();
+    const today = d.toLocaleString( 'sv' );
     $('input[name="dateLastUpdate"]').val(today);
+    
+    let notesVal =  $('input[name="notes"]').val();
+    $('input[name="notes"]').val(originalText + ': changed to: ' + newText + + '<br/>' + notesVal);
+
+    $("#order-submit").addClass("unhidden");
 }
